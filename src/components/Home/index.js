da618fbce217b941782400cb53b0f723b2f985e0
Ugochukwu Miracle <ugwumiracle123@gmail.com>
2023-06-21 08:52:50 +0100

    fixed naming error

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ import Loader from 'react-loaders'
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = ' Ugochukwu'.split('')
-  const jobArray = 'Software Developer'.split('')
+  const jobArray = 'Software Dev'.split('')
 
   useEffect(() => {
     setTimeout(() => {
